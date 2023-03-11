fce78fbe23cf818e00e40e2e764fb3612cd108f0
msrepo <maheshshakya.01@gmail.com>
2023-03-11 10:33:40 +0545

    change label

--- xrayto3d_morphometry/sitk_utils.py
+++ xrayto3d_morphometry/sitk_utils.py
@@ -1,4 +1,6 @@
 import SimpleITK as sitk
+from typing import Sequence, Optional
+import vedo
 
 def get_segmentation_labels(segmentation:sitk.Image):
     fltr = get_segmentation_stats(segmentation)
@@ -10,3 +12,59 @@ def get_segmentation_stats(segmentation: sitk.Image)->sitk.LabelShapeStatisticsI
     fltr.ComputeFeretDiameterOn()
     fltr.Execute(segmentation)
     return fltr
+
+def change_label(img: sitk.Image, mapping_dict) -> sitk.Image:
+    """use SimplITK AggregateLabelMapFilter to merge all segmentation labels to first label. This is used to obtain the bounding box of all the labels """
+    fltr = sitk.ChangeLabelImageFilter()
+    fltr.SetChangeMap(mapping_dict)
+    return fltr.Execute(sitk.Cast(img,sitk.sitkUInt8))
+
+def get_segmentation_volume(filename,label_id,largest_component=False):
+    sitk_volume = sitk.ReadImage(filename)
+    labels = get_segmentation_labels(sitk_volume)
+
+    # get segmentation volume with specific label id 
+    change_map_dict = {}
+    for  l in labels:
+        if l == label_id:
+            change_map_dict[l] = 1
+        else:
+            change_map_dict[l] = 0
+    sitk_volume = change_label(sitk_volume,change_map_dict)  
+
+    if largest_component:
+        # get largest connected component
+        sitk_volume = sitk.RelabelComponent(sitk.ConnectedComponent(
+            sitk.Cast(sitk_volume,sitk.sitkUInt8),
+            ),sortByObjectSize=True) == 1
+              
+    return vedo.Volume(sitk.GetArrayFromImage(sitk_volume))
+
+def make_isotropic(img: sitk.Image, spacing:Optional[float]=None, interpolator= sitk.sitkNearestNeighbor):
+    '''
+    Resample `img` so that the voxel is isotropic with given physical spacing
+    The image volume is shrunk or expanded as necessary to represent the same physical space. 
+    Use sitk.sitkNearestNeighbour while resampling Label images,
+    when spacing is not supplied by the user, the highest resolution axis spacing is used
+    '''
+    # keep the same physical space, size may shrink or expand
+
+    if spacing is None:
+        spacing = min(list(img.GetSpacing()))
+
+
+    resampler = sitk.ResampleImageFilter()
+    new_size = [round(old_size * old_spacing / spacing)
+                for old_size, old_spacing in zip(img.GetSize(), img.GetSpacing())]
+    output_spacing = [spacing]*len(img.GetSpacing())
+
+    resampler.SetOutputSpacing(output_spacing)
+    resampler.SetSize(new_size)
+    resampler.SetOutputDirection(img.GetDirection())
+    resampler.SetOutputOrigin(img.GetOrigin())
+    resampler.SetTransform(sitk.Transform())
+    resampler.SetInterpolator(interpolator)
+    resampler.SetDefaultPixelValue(
+        img.GetPixelIDValue())
+
+    return resampler.Execute(img)

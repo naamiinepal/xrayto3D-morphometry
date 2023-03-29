335afb722fe83a9cae85863f0cc175e5051b86e0
mahesh shakya <mahesh.shakya@naamii.org.np>
2023-03-29 10:59:16 +0545

    add reqd packages

--- setup.py
+++ setup.py
@@ -1,28 +1,31 @@
 from setuptools import find_packages, setup
 
-setup(name='xrayto3d_morphometry',
-      version="0.1.0",
-      description="Preprocessing utils and scripts for Dataset creation for X-ray to 3D Shape Reconstruction",
-      url="https://github.com/naamiinepal/xrayto3D_morphometry",
-      author="Mahesh Shakya",
-      author_email="mahesh.shakya@naamii.org.np",
-      packages=find_packages(exclude=['build','docs']),
-      install_requires=[
-            'simpleitk<2.1.0', # temporary fix ITK ERROR: ITK only supports orthonormal direction cosines. No orthonormal definition found! 
-            'omegaconf',
-            'nibabel',
-            'pandas',
-            'vedo'
-      ],
-      keywords="Python utilities simpleitk biomedical",
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Intended Audience :: Science/Research',
-                   'Topic :: Scientific/Engineering :: Medical Science Apps.',
-
-                   # Pick your license as you wish (should match "license" above)
-                   'License :: OSI Approved :: MIT License',
-
-                   # Specify the Python versions you support here. In particular, ensure
-                   # that you indicate whether you support Python 2, Python 3 or both.
-                   'Programming Language :: Python :: 3.6'
-                   ])
+setup(
+    name="xrayto3d_morphometry",
+    version="0.1.0",
+    description="Preprocessing utils and scripts for Dataset creation for X-ray to 3D Shape Reconstruction",
+    url="https://github.com/naamiinepal/xrayto3D_morphometry",
+    author="Mahesh Shakya",
+    author_email="mahesh.shakya@naamii.org.np",
+    packages=find_packages(exclude=["build", "docs"]),
+    install_requires=[
+        "simpleitk<2.1.0",  # temporary fix ITK ERROR: ITK only supports orthonormal direction cosines. No orthonormal definition found!
+        "omegaconf",
+        "nibabel",
+        "pandas",
+        "vedo",
+        "point_cloud_utils",
+        "cma",  # for optimization using covariance-matrix adaption
+    ],
+    keywords="Python utilities simpleitk biomedical",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: MIT License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3.6",
+    ],
+)

9daad85fa89158b9278ebd8cc94c55a299b3b71e
mahesh shakya <mahesh.shakya@naamii.org.np>
2023-05-11 18:17:08 +0545

    fix: add psis_r

--- process_hip_csv.py
+++ process_hip_csv.py
@@ -1,5 +1,6 @@
 import argparse
 import pandas as pd
+from pathlib import Path
 
 from xrayto3d_morphometry import get_distance_between_points
 
@@ -8,23 +9,24 @@ parser.add_argument('csv_file')
 
 args = parser.parse_args()
 
-df = pd.read_csv(args.csv_file, header=None)
+df = pd.read_csv(args.csv_file)
 header = 'id,ASIS_L,ASIS_R,PT_L,PT_R,IS_L,IS_R,PSIS_L,PSIS_R\n'
 
 
-def get_formatted_row(id,landmarks):
-    assert len(landmarks) == 7
-    return f'{id},{landmarks[0]:.3f},{landmarks[1]:.3f},{landmarks[2]:.3f},{landmarks[3]:.3f},{landmarks[4]:.3f},{landmarks[5]:.3f},{landmarks[6]:.3f}\n'
+def get_formatted_row(id, landmarks):
+    assert len(landmarks) == 8
+    return f'{id},{landmarks[0]:.3f},{landmarks[1]:.3f},{landmarks[2]:.3f},{landmarks[3]:.3f},{landmarks[4]:.3f},{landmarks[5]:.3f},{landmarks[6]:.3f},{landmarks[7]:.3f}\n'
 
 
-with open('pelvic_landmark_error.csv', 'w', encoding='utf-8') as f:
+outfile = Path(args.csv_file).with_name('hip_landmark_error.csv')
+with open(str(outfile), 'w', encoding='utf-8') as f:
     f.write(header)
-    ids = df[0].unique()
+    ids = df['id'].unique()
 
     for id in ids:
-        rows = df[df[0] == id].to_numpy()
-        assert len(rows) == 2, f'exactly 2 rows expected'
+        rows = df[df['id'] == id].to_numpy()
+        assert len(rows) == 2, 'exactly 2 rows expected'
         gt_row = rows[0][2:]
         pred_row = rows[1][2:]
-        diff = [get_distance_between_points(gt_row[i*3:i*3+3],pred_row[i*3:i*3+3]) for i in range(7)]
+        diff = [get_distance_between_points(gt_row[i*3:i*3+3],pred_row[i*3:i*3+3]) for i in range(8)]
         f.write(get_formatted_row(id, diff))

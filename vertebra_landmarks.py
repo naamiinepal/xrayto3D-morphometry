806126d4b69ac9180abf07260ad1acc470f36c9a
msrepo <maheshshakya.01@gmail.com>
2023-05-15 11:19:28 +0545

    L1 regularization: tested but not going to use

--- vertebra_landmarks.py
+++ vertebra_landmarks.py
@@ -1,9 +1,11 @@
 from typing import Sequence
+from pathlib import Path
 import vedo
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from sklearn.linear_model import QuantileRegressor
 from xrayto3d_morphometry import (
     get_mesh_from_segmentation,
     move_to_origin,
@@ -23,6 +25,19 @@ from xrayto3d_morphometry import (
     get_oriented_camera
     
 )
+
+def fit_l1(data, alpha=0.2):
+    """fit a L1 regularized linear model
+    data: (N,3) where N is number of data points
+    alpha: regularization strength
+    """
+    quantile_reg = QuantileRegressor(solver='interior-point', alpha=alpha).fit(data[:, :2], data[:, 2])
+    fitted_data = []
+    for datum in data:
+        x, y, z = datum
+        z_new, = quantile_reg.predict([[x, y],]).flatten().tolist()
+        fitted_data.append([x, y, z_new])
+    return fitted_data
 
 
 def get_axis_lines(origin, boundary_points, scale=40):
@@ -67,7 +82,7 @@ def get_symmetry_plane(vert_mesh):
     sampled_vmp = vert_mesh.points()[rand_idx]
     sampled_apts1 = aligned_pts1.points()[rand_idx]
     avg_points = [lerp(a, b, 0.5) for a, b in zip(sampled_vmp, sampled_apts1)]
-    sym_plane = vedo.fit_plane(avg_points)
+    sym_plane = vedo.fit_plane(avg_points, signed=True)
     return sym_plane
 
 
@@ -103,9 +118,16 @@ def get_fitted_line_along_z(sup_or_inf_line: vedo.Line, boundary_points: vedo.Po
     return sup_or_inf_line
 
 
-sample_file = "test_data/sub-verse004_vert-24-seg-vert_msk.nii.gz"
 
-vert_mesh = get_mesh_from_segmentation(sample_file,largest_component=True)
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('nifti_file')
+parser.add_argument('--offscreen',default=False,action='store_true')
+args = parser.parse_args()
+
+sample_file = args.nifti_file
+
+vert_mesh = get_mesh_from_segmentation(sample_file,largest_component=True,reorient=True,orientation='ASL')
 move_to_origin(vert_mesh)
 # initial orientation
 vert_ctd = vert_mesh.center_of_mass()
@@ -125,7 +147,9 @@ sym_plane = get_symmetry_plane(vert_mesh)
 # plt.interactive().close()
 # sys.exit()
 
-sym_plane_boundaries = vert_mesh.clone(transformed=True).cut_with_plane(normal=(sym_plane.normal)).boundaries()
+cut_mesh = vert_mesh.clone(transformed=True).cut_with_plane(normal=(sym_plane.normal))
+sym_plane_boundaries = cut_mesh.boundaries()
+
 sym_plane_points = sym_plane_boundaries.points().tolist()
 # use kmeans to sepearte the vertebral body and spinous process boundary points
 kmeans = KMeans(n_clusters=2, random_state=0, n_init="auto").fit(sym_plane_points)
@@ -134,6 +158,8 @@ c1_x, c1_y, c1_z = kmeans.cluster_centers_[1]
 vb_label = 1 if c1_z < c0_z else 0
 sp_label = int(not bool(vb_label))
 vertebral_body_points = [p for p in sym_plane_points if kmeans.predict([p,])[0] == vb_label]
+# smooth vertebral body points
+vertebral_body_points = [np.mean(sym_plane_boundaries.closest_point(p, n=10), axis=0).tolist() for p in vertebral_body_points]
 spinous_process_points = [p for p in sym_plane_points if kmeans.predict([p,])[0] == sp_label]
 
 vbc = np.mean(vertebral_body_points, axis=0)
@@ -163,8 +189,31 @@ vb_anteroposterior = [vertebral_body_points[i] for i, vbnp in enumerate(vbn_proj
 
 # separate endplates
 m, c = get_slope_intercept_from_two_points_z_y(*vb_axis1)
+vb_endplate_label = [1 if (z*m+c) > y else 0 for x, y, z in vb_endplate]
+vb_endplate_pcl = vedo.Points(vb_endplate)
+
+# smoothed_vb_endplate_label = []
+# for p in vb_endplate:
+#     ids = vb_endplate_pcl.closest_point(p, n=50, return_point_id=True)
+#     labels = [vb_endplate_label[int(i)] for i in ids]
+#     smoothed_vb_endplate_label.append(int(np.mean(labels)))
+
+# vb_up = np.asarray([vb_endplate[i] for i, l in enumerate(smoothed_vb_endplate_label) if l > 0.5])
+# vb_lp = np.asarray([vb_endplate[i] for i, l in enumerate(smoothed_vb_endplate_label) if l <= 0.5])
 vb_up = np.asarray([(x, y, z) for x, y, z in vb_endplate if (z*m+c) > y])
 vb_lp = np.asarray([(x, y, z) for x, y, z in vb_endplate if (z*m+c) < y])
+# fit l1-regularized line
+# vb_up = fit_l1(vb_up)
+# vb_lp = fit_l1(vb_lp)
+
+#separate anteroposterior boundaries
+m, c = get_slope_intercept_from_two_points_y_z(*vb_axis2)
+vb_ap = np.asarray([(x, y, z) for x, y, z in vb_anteroposterior if (y*m+c) > z])
+vb_pp = np.asarray([(x, y, z) for x, y, z in vb_anteroposterior if (y*m+c) < z])
+# fit L1-regularized line
+# vb_ap = fit_l1(vb_ap)
+# vb_pp = fit_l1(vb_pp)
+
 a_bs = vedo.fit_line(np.asarray(vb_up))
 a_bi = vedo.fit_line(np.asarray(vb_lp))
 # update projectin lines to stay flush
@@ -177,10 +226,7 @@ a_bm_0 = lerp(a_bs_0, a_bi_0, 0.5)
 a_bm_1 = lerp(a_bs_1, a_bi_1, 0.5)
 a_bm = vedo.fit_line(np.asarray([a_bm_0, a_bm_1]))
 
-#separate anteroposterior boundaries
-m, c = get_slope_intercept_from_two_points_y_z(*vb_axis2)
-vb_ap = np.asarray([(x, y, z) for x, y, z in vb_anteroposterior if (y*m+c) > z])
-vb_pp = np.asarray([(x, y, z) for x, y, z in vb_anteroposterior if (y*m+c) < z])
+
 a_ba = vedo.fit_line(np.asarray(vb_ap))
 a_bp = vedo.fit_line(np.asarray(vb_pp))
 
@@ -236,13 +282,13 @@ print(f'vcl {vcl:.2f}')
 topview_cam = get_oriented_camera(vert_mesh, axis= 1, camera_dist = -200)
 topview_cam['viewup'] = (-1, 0, 0)
 
-sideview_cam = get_oriented_camera(vert_mesh, axis = 0, camera_dist = -200)
-sideview_cam['viewup'] = (0,-1,0)
+sideview_cam = get_oriented_camera(vert_mesh, axis = 0, camera_dist = 200)
+sideview_cam['viewup'] = (0,1,0)
 
 vedo.show(
-    # vert_mesh.c("white", 0.2),
-    vert_mesh.cut_with_plane(normal=sym_plane.normal,invert=True).c('white',alpha=0.5),
-    vedo.Plane(normal=(1,0,0), s=(80, 80), alpha=0.5, c='green'),
+    # vert_mesh.c("white",1.0),
+    vert_mesh.clone(transformed=True).cut_with_plane(normal=sym_plane.normal,invert=True).c('white',alpha=0.5),
+    # vedo.Plane(normal=(1,0,0), s=(80, 80), alpha=0.5, c='green'),
     # sym_plane_boundaries,
     vedo.Points((vert_ctd, vbc, spc, v0, s0,),r=10),
     # vedo.Points(vb_endplate, r=8, c=(1.,0.,0.)),
@@ -253,24 +299,23 @@ vedo.show(
     # # vedo.Points(project_spp, r=3, c=(1.,0.,0.)),
     vedo.Point(posterior_most_spp, r=8, c='red'),
     vedo.Point(anterior_most_spp, r=8, c='red'),
-    vedo.Line(vb_axis1),
+    # vedo.Line(vb_axis1),
     vedo.Line(sp_axis1),
     vedo.Line(v0, s0),
-    a_bs.lw(10), a_bi.lw(10),
-    # a_ba.lw(10), a_bp.lw(10),
-    # vedo.Points(a_bs.points(), r=8, c='white'),
-    # vedo.Points(a_bi.points(), r=8, c='white'),
-    # vedo.Points(a_ba.points(), r=8, c='white'), 
-    # vedo.Points(a_bp.points(), r=8, c='white'),
+    a_bs.lw(5), a_bi.lw(5),
+    vedo.Points(a_bs.points(), r=8, c='white'),
+    vedo.Points(a_bi.points(), r=8, c='white'),
+    vedo.Points(a_ba.points(), r=8, c='white'), 
+    vedo.Points(a_bp.points(), r=8, c='white'),
     # vedo.Point(a_ba.center, r=12, c='white'),
     # vedo.Point(a_bp.center, r=12, c='white'),
-    # vedo.Points(anterior_up_proj, r=4, c='red'),
-    # vedo.Points(anterior_down_proj, r=4, c='green'),
-    # vedo.Point(anterior_down_most_proj, r=12, c='white'),
-    # vedo.Point(anterior_up_most_proj, r=12, c='white'),
-    a_ba.lw(10), a_bp.lw(10),
+    a_ba.lw(5), a_bp.lw(5),
     axes=1,
-    camera=topview_cam,
-    resetcam=False
-
+    camera=sideview_cam,
+    resetcam=False,
+    offscreen=args.offscreen
 )
+
+outfile = Path('/mnt/driveD/repos/xrayto3D-morphometry/vertebra_debug_vis/sample.png').with_name(f'{Path(args.nifti_file).stem}.png')
+print(outfile)
+vedo.screenshot(str(outfile))

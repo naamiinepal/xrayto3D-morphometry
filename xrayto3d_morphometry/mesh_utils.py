0fe96a867707a8a9fb5793d9245b4cb613b74d59
msrepo <maheshshakya.01@gmail.com>
2023-08-24 08:21:01 +0545

    smooth mesh after marching cube

--- xrayto3d_morphometry/mesh_utils.py
+++ xrayto3d_morphometry/mesh_utils.py
@@ -5,6 +5,7 @@ import SimpleITK as sitk
 import vedo
 
 from .sitk_utils import make_isotropic
+from .geom_ops import lerp
 
 
 def get_principal_axis(mesh_obj: vedo.Mesh) -> Tuple[np.ndarray, vedo.Ellipsoid]:
@@ -24,12 +25,13 @@ def align_along_principal_axes(mesh_obj) -> Tuple[vedo.Mesh, np.ndarray]:
     return aligned_mesh_obj, T
 
 
-def get_mesh_from_segmentation(filename: str, largest_component=False, flying_edges=True, decimate=False, decimation_ratio=1.0, isosurface_value=1.0, reorient=False, orientation='PIR') -> vedo.Mesh:
+def get_mesh_from_segmentation(filename: str, largest_component=False, flying_edges=True, decimate=False, decimation_ratio=1.0, isosurface_value=1.0, smooth=20, reorient=False, orientation='PIR') -> vedo.Mesh:
     np_volume = get_volume(filename, largest_component, reorient=reorient, orientation=orientation)
 
     # isosurface_values = get_segmentation_labels(sitk_volume)
     mesh_obj: vedo.Mesh = np_volume.isosurface(value=isosurface_value-0.1, flying_edges=flying_edges)
     mesh_obj = mesh_obj.fill_holes()
+    mesh_obj.smooth(niter=smooth)
     if decimate:
         mesh_obj = mesh_obj.decimate(fraction=decimation_ratio)
     return mesh_obj.cap()
@@ -61,3 +63,26 @@ def get_pointcloud_from_mesh(mesh_obj: vedo.Mesh, label, label_name='scalars'):
     point_cloud: np.ndarray = mesh_obj.clone(transformed=True).points()
     point_labels: np.ndarray = mesh_obj.pointdata[label_name]
     return vedo.Points(point_cloud[point_labels == label])
+
+
+def get_symmetry_plane(mesh_obj, sampled_points=100):
+    '''
+    get symmetry plane by mirroring the mesh obj and then registering
+    the vertices between the original and mirrored mesh obj.
+    Take average direction between 100 random registered points by
+    fitting a plane through these sampled points
+    '''
+    mirrored_vert_mesh = mesh_obj.clone(deep=True, transformed=True).mirror("x")
+    mirrored_vert_points = vedo.Points(mirrored_vert_mesh.points())
+    vert_mesh_points = vedo.Points(
+        mesh_obj.clone(deep=True, transformed=True).points()
+    )
+    aligned_pts1 = mirrored_vert_points.clone().align_to(vert_mesh_points, invert=False)
+
+    # draw arrows to see where points end up
+    rand_idx = np.random.randint(0, len(mesh_obj.points()), sampled_points)
+    sampled_vmp = mesh_obj.points()[rand_idx]
+    sampled_apts1 = aligned_pts1.points()[rand_idx]
+    avg_points = [lerp(a, b, 0.5) for a, b in zip(sampled_vmp, sampled_apts1)]
+    sym_plane = vedo.fit_plane(avg_points, signed=True)
+    return sym_plane
